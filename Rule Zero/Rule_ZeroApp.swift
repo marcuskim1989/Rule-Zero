505fdb1879d4b3e6417176c3af190b6de7d03996
Marcus Kim <marcuskim1989@gmail.com>
2023-05-15 19:42:54 -0700

    Initial Commit

---
+++ Rule Zero/Rule_ZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rule_ZeroApp.swift
+//  Rule Zero
+//
+//  Created by Marcus Y. Kim on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Rule_ZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
